725c2bf4034a1a0d5de765b4585a2ee38db2eda3
RchrdHndrcks <nizfrancod@gmail.com>
2025-05-05 22:46:25 -0300

    Wrap error instead of creating a new one

--- tracer_test.go
+++ tracer_test.go
@@ -11,19 +11,51 @@ func thisAlwaysFails() error {
 	return errors.New("always fails")
 }
 
+type customError struct {
+	message string
+}
+
+func (e *customError) Error() string {
+	return e.message
+}
+
 func TestTrace(t *testing.T) {
-	err := thisAlwaysFails()
-	if err == nil {
-		t.Error("expected error")
-	}
+	t.Run("trace", func(t *testing.T) {
+		err := thisAlwaysFails()
+		if err == nil {
+			t.Error("expected error")
+		}
 
-	tracedErr := stacktracer.Trace(err)
-	if tracedErr == nil {
-		t.Error("expected traced error")
-	}
+		tracedErr := stacktracer.Trace(err)
+		if tracedErr == nil {
+			t.Error("expected traced error")
+		}
 
-	expectedErrMsg := "tracer_test.go:20 - always fails"
-	if tracedErr.Error() != expectedErrMsg {
-		t.Errorf("expected %q, got %q", expectedErrMsg, tracedErr.Error())
-	}
+		expectedErrMsg := "tracer_test.go:20 - always fails"
+		if tracedErr.Error() != expectedErrMsg {
+			t.Errorf("expected %q, got %q", expectedErrMsg, tracedErr.Error())
+		}
+	})
+
+	t.Run("wrapps custom error", func(t *testing.T) {
+		err := &customError{message: "always fails"}
+		tracedErr := stacktracer.Trace(err)
+		if tracedErr == nil {
+			t.Error("expected traced error")
+		}
+
+		expectedErrMsg := "tracer_test.go:42 - always fails"
+		if tracedErr.Error() != expectedErrMsg {
+			t.Errorf("expected %q, got %q", expectedErrMsg, tracedErr.Error())
+		}
+
+		if !errors.Is(tracedErr, err) {
+			t.Error("expected traced error to be the same as the original error")
+		}
+
+		var customErr *customError
+		if !errors.As(tracedErr, &customErr) {
+			t.Error("expected traced error to be the same as the original error")
+		}
+	})
 }
